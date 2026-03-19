8f6400c59e8f3ef8eeebe2a89dff16a29c814cc5
yusuff <ysfnc27@gmail.com>
2026-03-19 23:15:07 +0300

    repo-db fixes

--- src/main/java/com/ecom/user/favourites/persistence/repository/FavouritesRepository.java
+++ src/main/java/com/ecom/user/favourites/persistence/repository/FavouritesRepository.java
@@ -10,12 +10,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface FavouritesRepository extends JpaRepository<Favourites, FavouritesId> {
+    
+    List<Favourites> findById_UserId(UUID idUserId);
 
-    List<Favourites> findByUserId(UUID userId);
+    List<Favourites> findById_ProductId(UUID productId);
 
-    List<Favourites> findByProductId(UUID productId);
-
-    Optional<Favourites> findByUserIdAndProductId(UUID userId, UUID productId);
-
-    UUID user(Users user);
+    Optional<Favourites> findById_UserIdAndId_ProductId(UUID userId, UUID productId);
 }
